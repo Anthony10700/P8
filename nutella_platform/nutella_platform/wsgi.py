58f6a158bdb6dec63e7155633843907d3dc37045
Anthony10700 <anthony.thillerot@gmail.com>
2021-01-08 18:40:12 +0100

    test2

--- nutella_platform/nutella_platform/wsgi.py
+++ nutella_platform/nutella_platform/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nutella_platform.settings')
+
+
+if os.environ.get('ENV') == 'PRODUCTION':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nutella_platform.nutella_platform.settings')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nutella_platform.settings')
 
 application = get_wsgi_application()
