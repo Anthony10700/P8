cc7bf36262cdda0ff5eab08b5fba74b0d8684951
Anthony10700 <anthony.thillerot@gmail.com>
2021-01-08 12:16:51 +0100

    Update admin.py
    add docstring

--- nutella_platform/purbeurre/admin.py
+++ nutella_platform/purbeurre/admin.py
@@ -1,13 +1,24 @@
+"""Class of admin panel
+"""
 from django.contrib import admin
 from purbeurre.models import Product, Categories
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    """admin product panel
+
+    Args:
+        admin ([type]): [description]
+    """
     search_fields  = ['name', 'id']
     list_filter = ['categories','save_product']
 
-
 @admin.register(Categories)
 class CategoriesAdmin(admin.ModelAdmin):
-    search_fields  = ['name', 'id']
+    """admin categories panel
 
+    Args:
+        admin ([type]): [description]
+    """
+    search_fields  = ['name', 'id']
+    
