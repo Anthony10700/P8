95cdf6159fa065eeba28dfeec404fdbee5df6af4
Anthony10700 <anthony.thillerot@gmail.com>
2021-01-08 16:32:16 +0100

    heroku

--- nutella_platform/nutella_platform/settings.py
+++ nutella_platform/nutella_platform/settings.py
@@ -128,20 +128,14 @@ INTERNAL_IP = ['127.0.0.1']
 
 if os.environ.get('ENV') == 'PRODUCTION':
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
-    # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
         os.path.join(PROJECT_ROOT, 'static'),
     )
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     DATABASES = {'default':{}}
-    # Static files settings
-    
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
-    
 else:
     DATABASES = {
     'default': {
