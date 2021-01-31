840a1c322d5d2ee8929f3576c5df9d255c13c900
Anthony10700 <anthony.thillerot@gmail.com>
2021-01-31 17:55:46 +0100

    test views

--- purbeurre/tests/test_views.py
+++ purbeurre/tests/test_views.py
@@ -8,7 +8,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 firefox_options = webdriver.FirefoxOptions()
-firefox_options.add_argument('--window-size=1920,1080') 
+
+firefox_options.add_argument('--headless')
+firefox_options.add_argument('--no-sandbox')
+firefox_options.add_argument('--disable-dev-shm-usage')
+firefox_options.add_argument('--window-size=1920,1080')
 firefox_options.headless = True
 
 
