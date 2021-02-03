9526a7c1c8f576933f4cc7c356e099ab4f66766c
Anthony10700 <anthony.thillerot@gmail.com>
2021-02-03 11:02:17 +0100

    new tests

--- purbeurre/tests/test_views.py
+++ purbeurre/tests/test_views.py
@@ -13,6 +13,7 @@ from selenium.webdriver.common.keys import Keys
 firefox_options = webdriver.FirefoxOptions()
 firefox_options.headless = True
 
+
 class UrlPurbeurreTests(TestCase):
     """
     Class test of url of app
@@ -20,6 +21,41 @@ class UrlPurbeurreTests(TestCase):
     Args:
         TestCase ([type]): [description]
     """
+    @classmethod
+    def setUpTestData(cls):
+        """This method make a account for testing the url form sign_in
+        """       
+        browser = webdriver.Firefox(options=firefox_options)
+        print("\nCreation d'un compte\n")
+        info = {"inputUsername": "Frost101",
+                "inputemail": "anthony.thillerot@laposte.nettt",
+                "inputPassword1": "azerty",
+                "inputPassword2": "azerty",
+                "inputNom": "Test_Nom",
+                "inputprenom": "Test_prenom"}
+
+        browser.get('http://127.0.0.1:8000/auth/sign_in.html')
+        grid = browser.find_element_by_id('inputemail')
+        grid.send_keys(info["inputemail"])
+        grid = browser.find_element_by_id('inputUsername')
+        grid.send_keys(info["inputUsername"])
+        grid = browser.find_element_by_id('inputPassword1')
+        grid.send_keys(info["inputPassword1"])
+        grid = browser.find_element_by_id('inputPassword2')
+        grid.send_keys(info["inputPassword2"])
+        grid = browser.find_element_by_id('inputNom')
+        grid.send_keys(info["inputNom"])
+        grid = browser.find_element_by_id('inputprenom')
+        grid.send_keys(info["inputprenom"])
+
+        browser.execute_script(
+            "document.getElementById('gridCheck').checked = true;")
+        time.sleep(1)
+        browser.execute_script(
+            "document.getElementsByClassName('btn btn-primary')[1].click();")
+
+        browser.quit()
+
     def setUp(self):
         """This method similar at __init__ for each instance
         """
@@ -58,41 +94,35 @@ class UrlPurbeurreTests(TestCase):
         time.sleep(2)
         self.browser.quit()
 
-    def test_connection_selenium(self):
-        """test connection with selenium
-        """
-        self.make_account()
+    def connect(self):
         self.browser.get('http://127.0.0.1:8000/auth/sign_in.html')
         inputusername = self.browser.find_element_by_id('inputEmail_connect')
-        inputusername.send_keys('anthony.thillerot@laposte.nett')
+        inputusername.send_keys('anthony.thillerot@laposte.nettt')
         inputpassword = self.browser.find_element_by_id(
             'inputPassword_connect')
         inputpassword.send_keys('azerty')
         # grid = self.browser.find_element_by_id('gridCheck_connect')
-        self.browser.execute_script("document.getElementById('gridCheck_connect').checked = true;")
+        self.browser.execute_script(
+            "document.getElementById('gridCheck_connect').checked = true;")
         # button = self.browser.find_element_by_id('button_valid_form')
-        self.browser.execute_script("document.getElementById('button_valid_form').click;")
-        time.sleep(5)
-        self.assertEqual(self.browser.title, "Bienvenue Frost10")
+        self.browser.execute_script(
+            "document.getElementById('button_valid_form').click();")
+
+    def test_connection_selenium(self):
+        """test connection with selenium
+        """
+        self.connect()
+        time.sleep(2)
+        self.assertEqual(self.browser.title, "Bienvenue Frost101")
         time.sleep(2)
         self.browser.quit()
 
     def test_seek_search_connection_selenium(self):
         """test searh connection with selenium
         """
-        self.make_account()
-        self.browser.get('http://127.0.0.1:8000/auth/sign_in.html')
-        inputusername = self.browser.find_element_by_id('inputEmail_connect')
-        inputusername.send_keys('anthony.thillerot@laposte.nett')
-        inputpassword = self.browser.find_element_by_id(
-            'inputPassword_connect')
-        inputpassword.send_keys('azerty')
-        grid = self.browser.find_element_by_id('gridCheck_connect')
-        grid.click()
-        button = self.browser.find_element_by_id('button_valid_form')
-        button.click()
-        time.sleep(5)
-        self.assertEqual(self.browser.title, "Bienvenue Frost10")
+        self.connect()
+        time.sleep(2)
+        self.assertEqual(self.browser.title, "Bienvenue Frost101")
 
         self.browser.get('http://127.0.0.1:8000/purbeurre/index.html')
         elem = self.browser.find_element_by_id('search-nav')
@@ -108,42 +138,13 @@ class UrlPurbeurreTests(TestCase):
     def test_show_product_selenium(self):
         """test show_product with selenium
         """
-        self.make_account()
-        self.browser.get('http://127.0.0.1:8000/auth/sign_in.html')
-        inputusername = self.browser.find_element_by_id('inputEmail_connect')
-        inputusername.send_keys('anthony.thillerot@laposte.nett')
-        inputpassword = self.browser.find_element_by_id(
-            'inputPassword_connect')
-        inputpassword.send_keys('azerty')
-        grid = self.browser.find_element_by_id('gridCheck_connect')
-        grid.click()
-
-        button = self.browser.find_element_by_id('button_valid_form')
-        button.click()
+        self.connect()
         time.sleep(5)
-        self.assertEqual(self.browser.title, "Bienvenue Frost10")
-
+        self.assertEqual(self.browser.title, "Bienvenue Frost101")
         self.browser.get(
             'http://127.0.0.1:8000/purbeurre/show_product.html/?id=8954&search=boisson')  # noqa: E501
         elem = self.browser.find_element_by_class_name(
             'card_description').find_elements_by_tag_name("h5")[0]
         self.assertEqual(elem.text, "Repères nutritionnels pour 100g :")
         time.sleep(2)
-        self.browser.quit()
-
-    def make_account(self):
-        """This method make a account for testing the url form sign_in
-        """       
-        info = {"inputUsername": "Frost10",
-                "inputemail": "anthony.thillerot@laposte.nett",
-                "inputPassword1": "azerty",
-                "inputPassword2": "azerty",
-                "inputNom": "Test_Nom",
-                "inputprenom": "Test_prenom"}
-        response = self.client.post('/auth/sign_in.html', data=info)
-        self.assertEqual(response.status_code, 302)
-
-        response = self.client.get('/auth/account.html')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(
-            response.context['account_info']["Email"], info["inputemail"])
+        self.browser.quit()      
