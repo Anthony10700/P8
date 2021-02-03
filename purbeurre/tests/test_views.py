a597f98e59ff0562ed20eba602faba8948671fc2
Anthony10700 <anthony.thillerot@gmail.com>
2021-02-03 12:23:06 +0100

    Update test_views.py

--- purbeurre/tests/test_views.py
+++ purbeurre/tests/test_views.py
@@ -7,6 +7,7 @@ from django.test import TestCase, Client
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from purbeurre.models import Product, Categories
+import json
 # from pyvirtualdisplay import Display 
 # display = Display(visible=0, size=(1024, 768)) 
 # display.start() 
@@ -56,6 +57,60 @@ class UrlPurbeurreTests(TestCase):
             "document.getElementsByClassName('btn btn-primary')[1].click();")
 
         browser.quit()
+
+        """this method create a object product
+        """
+        categories = Categories.objects.create(
+            name="boissons-a-la-canneberge",
+            url="https://fr.openfoodfacts.org/categorie/boissons-a-la-canneberge.json",  # noqa: E501
+            nb_of_products=int(54))
+
+        categories.save()
+        categories_fk = Categories.objects.get(name="boissons-a-la-canneberge")
+        product_nutriments = {
+            "fat_value": "0.5", "nova-group_100g": 4, "carbohydrates": "10.5",
+            "saturated-fat_value": "0.1", "fat_serving": 1, "fat_100g": "0.5",
+            "salt": 0, "sodium_value": 0, "fat": "0.5",
+            "carbohydrates_serving": 21, "sugars_value": "10.5",
+            "sodium_serving": 0, "salt_unit": "g",
+            "fruits-vegetables-nuts_serving": 25, "salt_value": 0,
+            "sodium": 0, "energy-kcal": 44,
+            "energy-kcal_serving": 88, "fruits-vegetables-nuts_100g": 25,
+            "saturated-fat_unit": "g", "nova-group_serving": 4,
+            "sodium_unit": "g", "proteins_unit": "g",
+            "energy_unit": "kcal", "salt_serving": 0,
+            "fruits-vegetables-nuts-estimate-from-ingredients_100g": 25,
+            "sodium_100g": 0, "sugars": "10.5", "energy_100g": 184,
+            "proteins_value": "0.5", "nova-group": 4,
+            "saturated-fat_serving": "0.2",
+            "saturated-fat_100g": "0.1",
+            "sugars_serving": 21,
+            "proteins_100g": "0.5", "energy": 184,
+            "carbohydrates_value": "10.5", "nutrition-score-fr_100g": 14,
+            "carbon-footprint-from-known-ingredients_100g": "7.5",
+            "carbon-footprint-from-known-ingredients_serving": 15,
+            "energy-kcal_100g": 44, "carbohydrates_100g": "10.5",
+            "salt_100g": 0, "fruits-vegetables-nuts_value": 25,
+            "carbohydrates_unit": "g", "saturated-fat": "0.1",
+            "carbon-footprint-from-known-ingredients_product": 75,
+            "proteins_serving": 1, "fruits-vegetables-nuts_label": "0",
+            "fruits-vegetables-nuts_unit": "g", "energy-kcal_value": 44,
+            "energy-kcal_unit": "kcal", "energy_serving": 368,
+            "nutrition-score-fr": 14, "sugars_100g": "10.5",
+            "fruits-vegetables-nuts": 25, "sugars_unit": "g",
+            "proteins": "0.5", "fat_unit": "g", "energy_value": 44}
+        product_bdd = Product.objects.create(
+            name="Cranberry",
+            countries="France",
+            id_openfoodfacts="3596710355051",
+            url="https://fr.openfoodfacts.org/produit/3596710355051/cranberry-auchan",  # noqa: E501
+            image_url="https://static.openfoodfacts.org/images/products/359/671/035/5051/front_fr.45.400.jpg",  # noqa: E501
+            store="Auchan",
+            nutriscore_grade="e",
+            categories=categories_fk,
+            nutriments=json.dumps(product_nutriments))
+
+        product_bdd.save()
 
     def setUp(self):
         """This method similar at __init__ for each instance
@@ -139,6 +194,7 @@ class UrlPurbeurreTests(TestCase):
     def test_show_product_selenium(self):
         """test show_product with selenium
         """
+        Product.object.get
         self.connect()
         time.sleep(5)
         self.assertEqual(self.browser.title, "Bienvenue Frost101")
@@ -149,3 +205,6 @@ class UrlPurbeurreTests(TestCase):
         self.assertEqual(elem.text, "Repères nutritionnels pour 100g :")
         time.sleep(2)
         self.browser.quit()      
+    
+
+        
